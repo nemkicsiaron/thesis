f474cbf0d9dfe3a2ff35d1f64624a56ea809160b
nemkicsiaron <aronfszabo@gmail.com>
2022-02-13 18:37:34 +0100

    Prisma schema

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,16 +1,8 @@
 import express from "express";
-import { open } from "sqlite";
-import sqlite3 from "sqlite3";
-
 
 const server: express.Application = express();
 const port: number = 3000;
-open({
-    filename: "./test.db",
-    driver: sqlite3.Database
-}).then((db) => {
 
-});
 server.listen(port,() => {
     console.log(`Server listening on port: ${port}`);
 });
