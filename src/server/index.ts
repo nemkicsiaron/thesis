97cd77c15990a86f186deb17b0ebaf486d0c195d
nemkicsiaron <aronfszabo@gmail.com>
2022-03-19 18:17:09 +0100

    updated modules

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,7 +1,12 @@
 import express from "express";
+import routes from "./routes";
 
 const server: express.Application = express();
 const port: number = 3000;
+
+
+server.use(express.json());
+server.use(routes)
 
 server.listen(port,() => {
     console.log(`Server listening on port: ${port}`);
