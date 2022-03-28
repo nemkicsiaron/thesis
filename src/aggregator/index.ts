019d2c46a832759f17d0e54b8d91c876983fe09c
nemkicsiaron <aronfszabo@gmail.com>
2022-03-28 23:33:37 +0200

    config and rate limiting

--- src/aggregator/index.ts
+++ src/aggregator/index.ts
@@ -1,15 +1,19 @@
 import express from "express";
+import { rateLimit, RateLimitRequestHandler } from "express-rate-limit";
 import routes from "./routes";
-import fetch from "node-fetch"
 import controller from "./auth/controller/user"
-import { Http2ServerRequest } from "http2";
 
 const aggregator: express.Application = express();
 const port: number = 4000;
+const limiter: RateLimitRequestHandler = rateLimit({
+    windowMs: 10 * 60 * 1000, // 10min
+    max: 100, // 100 req per `window` per IP
+    standardHeaders: true // return rate limit info as RateLimit-* header
+});
 
 aggregator.use(express.json());
-aggregator.use(routes)
-
+aggregator.use(routes);
+aggregator.use(limiter);
 
 aggregator.listen(port,() => {
     console.log(`Aggregator Server listening on port: ${port}`);
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,13 +1,37 @@
 import express from "express";
+import { rateLimit, RateLimitRequestHandler } from "express-rate-limit"
 import routes from "./routes";
+import { aggregatorUri } from "./util/config";
+import { listallcat } from "./functions/listallcat";
 
 const server: express.Application = express();
 const port: number = 3000;
+const limiter: RateLimitRequestHandler = rateLimit({
+    windowMs: 10 * 60 * 1000, // 10min
+    max: 100, // 100 req per `window` per IP
+    standardHeaders: true // return rate limit info as RateLimit-* header
+});
 
 
 server.use(express.json());
-server.use(routes)
+server.use(routes);
+server.use(limiter);
 
-server.listen(port,() => {
+server.listen(port, async () => {
+    try {
+        const discoveryaddr = aggregatorUri + "/discover";
+        console.log(discoveryaddr);
+        const body = {
+            address: "http://localhost:3000",
+            cat: await listallcat()
+        };
+        await fetch(discoveryaddr, {
+            method: "post",
+            body: JSON.stringify(body),
+            headers: {"Content-Type": "application/json"}
+        });
+    } catch (error) {
+        console.error(error);
+    }
     console.log(`Server listening on port: ${port}`);
 });
