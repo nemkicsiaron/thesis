ad91fbb58c4a28c0e34c1151739de7da2d3f7ff6
nemkicsiaron <aronfszabo@gmail.com>
2022-04-24 21:19:17 +0200

    crud update

--- src/aggregator/routes/aggreg.route.ts
+++ src/aggregator/routes/aggreg.route.ts
@@ -4,8 +4,8 @@ import listallcat from "../functions/listallcat";
 import { indexer, serverslist } from "../indexing/indexer";
 import Category from "../interfaces/category";
 import Post from "../interfaces/post";
-import generalquery from "../query/generalquery";
-import servercatquery from "../query/servercatquery";
+import generalquery from "../crud/generalquery";
+import servercatquery from "../crud/servercatquery";
 
 const aggregrouter = Router();
 
@@ -73,7 +73,7 @@ aggregrouter.post('/newpost', async (req, res) => {
         publish: data.post.publish,
         price: data.post.price,
         author: data.post.author,
-        //signature: data.post.signature,
+        signature: data.post.signature,
         description: data.post.description,
         created: new Date(),
         //categoryRelation: (await listallcat(serveraddr)).find(c => c.name === data.post.category)
@@ -89,5 +89,39 @@ aggregrouter.post('/newpost', async (req, res) => {
 
     res.status(200).json(await createres.json());
 });
+
+aggregrouter.delete('/deletepost', async (req, res) => {
+    const data = await req.body;
+    const newpost: Post = {
+        title : data.post.title,
+        category: data.post.category,
+        publish: data.post.publish,
+        price: data.post.price,
+        author: data.post.author,
+        signature: data.post.signature,
+        description: data.post.description,
+        created: data.post.created,
+    };
+
+    try {
+        const deleteres = await fetch(data.server?.toString() + "/api/deletepost", {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json"
+                },
+            body: JSON.stringify(newpost)
+        });
+        res.status(200).json(await deleteres.json());
+    } catch(error: any) {
+        console.error(error);
+        res.status(404).json(error.message);
+    }
+});
+
+aggregrouter.put('/updatepost', async (req, res) => {
+
+});
+
+
 
 export default aggregrouter;
