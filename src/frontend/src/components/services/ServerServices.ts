390e0d102a592e3d0af4ce61822cdb55742e2a17
nemkicsiaron <aronfszabo@gmail.com>
2022-04-29 18:56:38 +0200

    List all servers from aggreg

---
+++ src/frontend/src/components/services/ServerServices.ts
@@ -0,0 +1,15 @@
+import IServer from "../../interfaces/servers";
+import { aggregatorUri } from "../../config/config";
+
+export default async function listAllServers(): Promise<IServer[]> {
+
+    const result = await fetch(`${aggregatorUri}/aggreg/alldb`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+    const servers: IServer[] = await result.json();
+    return servers;
+
+}
