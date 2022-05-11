b5418ae5cee405dea32812908253a0f45891b694
nemkicsiaron <aronfszabo@gmail.com>
2022-05-11 14:12:42 +0200

    Major Profile Management UX update

--- src/frontend/src/routes/ProfilePage.tsx
+++ src/frontend/src/routes/ProfilePage.tsx
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { Button, ListGroup } from "reactstrap";
 import { Failed, useLogout } from "../hooks/LoginHooks";
-import LoginProvider, { LoggedIn, LoginContext } from "../components/contexts/LoginProvider";
+import LoginProvider, { LoggedIn, LoggedOut, LoginContext } from "../components/contexts/LoginProvider";
 
 import "./styles/Profile.scss";
 
@@ -56,13 +56,22 @@ const ProfilePage: React.FC = () => {
                             <p>{loginState.user.privatekey.length ? "Sikeresen betöltve" : "Nincs betöltve"}</p>
                     )}
                 </div>
-                <ListGroup className="mt-4 btn-list">
-                    <Button type="submit" className="btn list-btn" color="success" size="lg" tag={Link} to="/login">Profil betöltése</Button>
-                    <Button type="button" className="btn list-btn" color="info" size="lg" onClick={() => manage()}>Hirdetések kezelése</Button>
-                    <Button type="button" className="btn list-btn" color="info" size="lg" tag={Link} to="/register" >Regisztrálás</Button>
-                    <Button type="button" className="btn back-btn" color="danger" size="lg" onClick={logout}>Kijelentkezés</Button>
-                    <Button type="button" className="btn back-btn" color="danger" size="lg" tag={Link} to="/">Vissza a főoldalra</Button>
-                </ListGroup>
+                    {
+                        loginState instanceof LoggedOut && (
+                            <ListGroup className="mt-4 btn-list">
+                                <Button type="submit" className="btn list-btn" color="success" size="lg" tag={Link} to="/login">Profil betöltése</Button>
+                                <Button type="button" className="btn list-btn" color="info" size="lg" tag={Link} to="/register" >Regisztrálás</Button>
+                                <Button type="button" className="btn back-btn" color="danger" size="lg" tag={Link} to="/">Vissza a főoldalra</Button>
+                            </ListGroup>
+                    )}
+                    {
+                        loginState instanceof LoggedIn && (
+                        <ListGroup className="mt-4 btn-list">
+                            <Button type="button" className="btn list-btn" color="success" size="lg" onClick={() => manage()}>Hirdetések kezelése</Button>
+                            <Button type="button" className="btn back-btn" color="info" size="lg" onClick={logout}>Kijelentkezés</Button>
+                            <Button type="button" className="btn back-btn" color="danger" size="lg" tag={Link} to="/">Vissza a főoldalra</Button>
+                        </ListGroup>
+                    )}
             </div>
         </LoginProvider>
     );
