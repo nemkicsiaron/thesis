42b7245d7e1b03d2e1282387f5b3f6c05248136b
nemkicsiaron <aronfszabo@gmail.com>
2022-05-14 02:02:11 +0200

    Quickstart script

---
+++ src/win_quick_start.bat
@@ -0,0 +1,9 @@
+@echo off
+cd ".\frontend"
+start "Frontend" /min npm start
+cd "..\aggregator"
+start "Aggregator" /min npm run demon
+cd "..\server"
+start "Server" /min npm run demon
+echo "Everything started in watch mode"
+PAUSE
